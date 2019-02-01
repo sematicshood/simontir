c83582ebdb84e0c059941d165b77871f4fb606bd
Bima Pangestu <bimapangestu280@gmail.com>
2019-02-01 15:47:12 +0700

    up

--- src/views/ListFinalCheck.vue
+++ src/views/ListFinalCheck.vue
@@ -71,7 +71,7 @@ export default class LastFinalCheck extends Vue {
                 }).splice(0, 1);
 
                 this.regulars = res.data.results.filter((el: any) => {
-                    return el.antrian_service == 'Regular Service';
+                    return el.antrian_service == 'regular';
                 }).splice(0, 1);
             }
         });
