8ee49e618c143814de74c05216c0928646b12328
dennyard <sematics.info@gmail.com>
2019-06-13 17:13:34 +0700

    Update day until

--- src/api/payroll.ts
+++ src/api/payroll.ts
@@ -3,7 +3,7 @@ import api from './api';
 const get = async (day: any, month: any, year: any, day_until: any, month_until: any, year_until: any , type: string) => {
     return await api.get('/simontir/payroll', {
         params: {
-            day, month, year, day_until, month_until, day_until, year_until, type,
+            day, month, year, day_until, month_until, year_until, type,
         },
     })
 };
